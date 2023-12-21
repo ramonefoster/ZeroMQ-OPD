d510af1fdfa89ee680fd0ccadc2d414239cc1194
Ramon <82529131+ramonefoster@users.noreply.github.com>
2023-12-21 14:36:22 -0300

    Update client_sample.py

--- client_sample.py
+++ client_sample.py
@@ -5,20 +5,17 @@ subscriber = context.socket(zmq.SUB)
 subscriber.connect("tcp://200.131.64.237:7001")
 
 # Subscribe to multiple topics
-subscriber.setsockopt(zmq.SUBSCRIBE, b'/observingconditions/humidity')
-subscriber.setsockopt(zmq.SUBSCRIBE, b'/observingconditions/temperature')
-subscriber.setsockopt(zmq.SUBSCRIBE, b'/observingconditions/windspeed')
-subscriber.setsockopt(zmq.SUBSCRIBE, b'/observingconditions/winddirection')
-subscriber.setsockopt(zmq.SUBSCRIBE, b'/observingconditions/pressure')
+subscriber.setsockopt_string(zmq.SUBSCRIBE, "")
 
+print("Client Online")
 poller = zmq.Poller()
 poller.register(subscriber, zmq.POLLIN)
 
 while True:
     socks = dict(poller.poll(100))
     if socks.get(subscriber) == zmq.POLLIN:
-        message = subscriber.recv_multipart()
-        topic = message[0]
-        data = message[1]  # Assuming the data is received as the second part of the multipart message
-        print(f"Received: {topic.decode()} - {data.decode()}")
-    
+        message = subscriber.recv().decode()
+        # topic = message[0]
+        # data = message[1]  # Assuming the data is received as the second part of the multipart message
+        print(f"Received: {message}")
+
